2cac357af8ed16be2b483e232472ce9c9e724c0b
Ignacio Penas <ignacio.penas4b@gmail.com>
2015-05-20 16:13:46 +0200

    Quiz 20 "DB de usuarios y encriptacion de pw"

--- models/models.js
+++ models/models.js
@@ -32,27 +32,42 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 //Importar definicion de la tabla Comment
 var Comment = sequelize.import(path.join(__dirname,'comment'));
+//Importar la definicion de la tabla User
+var User = sequelize.import(path.join(__dirname,'user'));
 
+//Hacemos la asignacio 1 a N de los comentarios
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
+//Realizamos la asignacio de cada N a 1 de los Users
+Quiz.belongsTo(User);
+User.hasMany(Quiz);
+
 exports.Quiz = Quiz;//Exportar la definicion de la tabla Quiz
 exports.Comment = Comment;//Exportar la definicion de la tabla Comments
+exports.User = User;//Exportar la definicion de la tabla User
 
 //sequelize.sync() crea e inicializa tabla de preguntar en DB
 sequelize.sync().then(function() {
   //then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){
+  User.count().then(function (count){
     if(count === 0){
-      Quiz.create({
-          pregunta: 'Capital de Italia',
-          respuesta: 'Roma'
+      User.bulkCreate(
+        [{username: 'admin', password: '1234', isAdmin: true},
+         {username: 'pepe', password: '5678'}
+        ]
+      ).then(function(){
+        console.log('Tabla User inicializada');
+        Quiz.count().then(function (count){
+            if (count === 0) {
+              Quiz.bulkCreate(
+                [{pregunta: 'Capital de Italia', respuesta: 'Roma', UserId: 2},
+                 {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', UserId: 2}
+                ]
+              ).then(function(){console.log('Tabla quiz inicializada')})
+            };
+        });
       });
-      Quiz.create({
-          pregunta: 'Capital de Portugal',
-          respuesta: 'Lisboa'
-      })
-      .then(function(){console.log('Base de datos inicializada')})
     };
   });
 });
